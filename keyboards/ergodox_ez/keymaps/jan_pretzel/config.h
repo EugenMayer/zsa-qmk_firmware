c62f7faa30feec339acaeed325acae0b342d2baa
em <eugen.mayer@kontextwork.de>
2020-03-07 16:56:10 +0100

    Add readme, adjust firmware version, add deploy script

--- keyboards/ergodox_ez/keymaps/jan_pretzel/config.h
+++ keyboards/ergodox_ez/keymaps/jan_pretzel/config.h
@@ -3,7 +3,7 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
-#define FIRMWARE_VERSION u8"nmAOR/mzm9N"
+#define FIRMWARE_VERSION u8"vAqXl/gYdnM"
 
 /*
 * #####################################################################
